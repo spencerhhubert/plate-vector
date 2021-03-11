37185b0fe9a990d07359741dcb4d361360c42c05
spencerhhubert <spencerhhubert@gmail.com>
2021-03-11 05:24:05 -0800

    cleaned up and fully featured

--- brick.js
+++ brick.js
@@ -1,78 +1,140 @@
 let svgNS = "http://www.w3.org/2000/svg"
 class Plate {
-    constructor(plateObject) {
+    constructor(plateObject, fill, fillColor, stroke, strokeColor, borderWidth) {
+        this.fill = fill;
+        this.stroke = stroke;
+        this.fillColor = fillColor;
+        this.strokeColor = strokeColor;
         this.unit = 5;
         this.length = plateObject.length;
         this.height = plateObject.height;
         this.pattern = plateObject.pattern;
+        this.borderWidth = borderWidth;
         this.node = document.createElementNS(svgNS, 'svg');
-        this.node.setAttribute("viewBox", `0 0 ${this.length * this.unit} ${this.height * this.unit}`);        
+        this.node.setAttribute("viewBox", `0 0 ${this.length * this.unit + (this.borderWidth*2)} ${this.height * this.unit + (this.borderWidth*2)}`);
     }
-    generateStuds() {
-        let x = 1;
-        let y = 1;
+    generate() {
+        if (this.fill == true) {
+            this.generateFill();
+        }
+        if (this.stroke == true) {
+            this.generateStroke();
+        }
+        this.generateStuds()
+    }
+    generateFill() {
+        let x = 1
+        let y = 1
         for (let i=1; i<this.pattern.length + 1; i++) {
-            if (this.pattern[i - 1] == 1) {
-                let stud = document.createElementNS(svgNS, 'circle');
-                stud.setAttribute("cx", (this.unit * (x - 1)) + (this.unit / 2));
-                stud.setAttribute("cy", (this.unit * (y - 1)) + (this.unit / 2));
-                stud.setAttribute("r", this.unit * (5/16));
-                this.node.appendChild(stud)
-
+            if (this.pattern[i-1] == 1) {
+                let xPrime = ((x-1) * this.unit) + (this.borderWidth/2);
+                let yPrime = ((y-1) * this.unit) + (this.borderWidth/2);
+                let fill = document.createElementNS(svgNS, 'path');
+                let argument = `M ${xPrime} ${yPrime} L ${xPrime + this.unit} ${yPrime} L ${xPrime + this.unit} ${yPrime + this.unit} L ${xPrime} ${yPrime + this.unit}`;
+                fill.setAttribute("d", argument);
+                fill.setAttribute("fill", this.fillColor);
+                fill.setAttribute("stroke-width", this.borderWidth/2);
+                fill.setAttribute("stroke", this.fillColor);
+                fill.id = `fill${i}`
+                this.node.appendChild(fill);
+            }
+            if (x % this.length == 0) {
+                x = 1;
+                y++
+            } else {
+                x = x+1
+            }
+        }
+    }
+    generateStroke() {
+        let x = 1
+        let y = 1
+        for (let i=1; i<this.pattern.length + 1; i++) {
+            if (this.pattern[i-1] == 1) {
+                let xPrime = ((x-1) * this.unit) + (this.borderWidth/2);
+                let yPrime = ((y-1) * this.unit) + (this.borderWidth/2);
                 let top = {
                     "x": x,
                     "y": y - 1,
+                    "name": "top",
                 }
                 let right = {
                     "x": x + 1,
                     "y": y,
+                    "name": "right",
                 }
                 let bottom = {
                     "x": x,
                     "y": y + 1,
+                    "name": "bottom",
                 }
                 let left = {
                     "x": x - 1,
                     "y": y,
+                    "name": "left",
                 }
 
-                let xPrime = x-1;
-                let yPrime = y-1
-                
-                if (this.outOfRange(top) || this.isStud(top) == false) {
-                    let frameFragemnt = document.createElementNS(svgNS, 'path');
-                    let argument = `M ${(xPrime) * this.unit} ${(yPrime) * this.unit} L ${(xPrime) * this.unit + this.unit} ${(yPrime) * this.unit}`;
-                    frameFragemnt.setAttribute("d", argument);
-                    this.node.appendChild(frameFragemnt);
-                }
-
-                if (this.outOfRange(right) || this.isStud(right) == false) {
-                    let frameFragemnt = document.createElementNS(svgNS, 'path');
-                    let argument = `M ${(xPrime) * this.unit + this.unit} ${(yPrime) * this.unit} L ${(xPrime) * this.unit + this.unit} ${(yPrime) * this.unit + this.unit}`;
-                    frameFragemnt.setAttribute("d", argument);
-                    this.node.appendChild(frameFragemnt);
-                }
-
-                if (this.outOfRange(bottom) || this.isStud(bottom) == false) {
-                    let frameFragemnt = document.createElementNS(svgNS, 'path');
-                    let argument = `M ${(xPrime) * this.unit} ${(yPrime) * this.unit + this.unit} L ${(xPrime) * this.unit + this.unit} ${(yPrime) * this.unit + this.unit}`;
-                    frameFragemnt.setAttribute("d", argument);
-                    this.node.appendChild(frameFragemnt);
-                }
-
-                if (this.outOfRange(left) || this.isStud(left) == false) {
-                    let frameFragemnt = document.createElementNS(svgNS, 'path');
-                    let argument = `M ${(xPrime) * this.unit} ${(yPrime) * this.unit} L ${(xPrime) * this.unit} ${(yPrime) * this.unit + this.unit}`;
-                    frameFragemnt.setAttribute("d", argument);
-                    this.node.appendChild(frameFragemnt);
+                let sides = [top, right, bottom, left];
+                for (let j=0; j<sides.length; j++) {
+                    if (this.outOfRange(sides[j]) || this.isStud(sides[j]) == false) {
+                        let frameFragment;
+                        let argument;
+                        switch (sides[j].name) {
+                            case "top":
+                                frameFragment = document.createElementNS(svgNS, 'path');
+                                argument = `M ${xPrime - this.borderWidth/2} ${yPrime} L ${xPrime + this.unit + this.borderWidth/2} ${yPrime}`;
+                                break;
+                            case "right":
+                                frameFragment = document.createElementNS(svgNS, 'path');
+                                argument = `M ${xPrime + this.unit} ${yPrime - this.borderWidth/2} L ${xPrime + this.unit} ${yPrime + this.unit + this.borderWidth/2}`;
+                                break;
+                            case "bottom":
+                                frameFragment = document.createElementNS(svgNS, 'path');
+                                argument = `M ${xPrime - this.borderWidth/2} ${yPrime + this.unit} L ${xPrime + this.unit + this.borderWidth/2} ${yPrime + this.unit}`;
+                                break;
+                            case "left":
+                                frameFragment = document.createElementNS(svgNS, 'path');
+                                argument = `M ${xPrime} ${yPrime - this.borderWidth/2} L ${xPrime} ${yPrime + this.unit + this.borderWidth/2}`;
+                                break;
+                        }
+                        frameFragment.setAttribute("d", argument);
+                        frameFragment.setAttribute("stroke-width", this.borderWidth);
+                        frameFragment.setAttribute("stroke", this.strokeColor);
+                        frameFragment.id = `frameFragment${i.toString() + j.toString()}`
+                        this.node.appendChild(frameFragment);
+                    }
                 }
             }
-
-            if ((x) % (this.length) == 0) {
+            if (x % this.length == 0) {
                 x = 1;
                 y++
             } else {
-                x++
+                x = x+1
+            }
+        }
+    }
+    generateStuds() {
+        let x = 1
+        let y = 1
+        for (let i=1; i<this.pattern.length + 1; i++) {
+            if (this.pattern[i-1] == 1) {
+                let xPrime = ((x-1) * this.unit) + (this.borderWidth/2);
+                let yPrime = ((y-1) * this.unit) + (this.borderWidth/2);
+                let stud = document.createElementNS(svgNS, 'circle');
+                stud.setAttribute("cx", (xPrime + (this.unit/2)));
+                stud.setAttribute("cy", (yPrime + (this.unit/2)));
+                stud.setAttribute("r", this.unit * (5/16));
+                stud.setAttribute("fill", "none");
+                stud.setAttribute("stroke-width", this.borderWidth);
+                stud.setAttribute("stroke", this.strokeColor);
+                stud.id = `stud${i}`
+                this.node.appendChild(stud);
+            }
+            if (x % this.length == 0) {
+                x = 1;
+                y++
+            } else {
+                x = x+1
             }
         }
     }
@@ -96,56 +158,6 @@ class Plate {
         }
     }
 }
-let plate2x2 = {
-    "length": 2,
-    "height": 2,
-    "pattern": [
-        1, 1,
-        1, 1
-    ]
-}
-
-let plateCorner2x2 = {
-    "length": 2,
-    "height": 2,
-    "pattern": [
-        1, 0,
-        1, 1
-    ]
-}
-
-let plate2x4= {
-    "length": 2,
-    "height": 4,
-    "pattern": [
-        1, 1,
-        1, 1,
-        1, 1,
-        1, 1,
-    ]
-}
-
-let randomShape= {
-    "length": 4,
-    "height": 5,
-    "pattern": [
-        1, 1, 1, 1,
-        1, 0, 0, 1,
-        1, 1, 1, 1,
-        0, 1, 0, 1,
-        1, 0, 1, 0,
-    ]
-}
-
-let randomShape2 = {
-    "length": 5,
-    "height": 5,
-    "pattern": generatePattern(5, 5)
-}
-let spot2 = document.getElementById("brick2");
-let test01 = new Plate(randomShape2)
-test01.generateStuds()
-spot2.appendChild(test01.node)
 
 function generatePattern(length, width) {
     let patternLength = length * width;
@@ -160,3 +172,36 @@ function generatePattern(length, width) {
     }
     return pattern
 }
+
+let plate2x4= {
+    "length": 2,
+    "height": 4,
+    "pattern": [
+        1, 1,
+        1, 1,
+        1, 1,
+        1, 1,
+    ]
+}
+
+let arbitraryShape= {
+    "length": 4,
+    "height": 5,
+    "pattern": [
+        1, 1, 1, 1,
+        1, 0, 0, 1,
+        1, 1, 1, 1,
+        0, 1, 0, 1,
+        1, 0, 1, 0,
+    ]
+}
+
+let randomShape = {
+    "length": 5,
+    "height": 5,
+    "pattern": generatePattern(5, 5)
+}
+let plateNode = document.getElementById("plate");
+let plate = new Plate(randomShape, true, "white", true, "#f5f5f5", .3)
+plate.generate()
+plateNode.appendChild(plate.node)
